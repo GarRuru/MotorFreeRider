2e162f9a81363e418aea710a74f05e2de80ecf37
Nick Chen <nick92251@gmail.com>
2017-11-27 01:48:25 +0800

    Facebook API Update
    按鈕置換，重新設計
    （還是fail的狀態）

--- app/src/main/java/com/example/motorfreerider/MainActivity.java
+++ app/src/main/java/com/example/motorfreerider/MainActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -26,6 +27,7 @@ import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONObject;
 
@@ -33,55 +35,83 @@ import java.util.Arrays;
 
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
     private AccessToken accessToken;
-    private TextView ID;
+    private LoginButton loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ID = (TextView) findViewById(R.id.title_text);
         //初始化FacebookSdk，記得要放第一行，不然setContentView會出錯
-
         FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //宣告callback Manager
-
         callbackManager = CallbackManager.Factory.create();
 
-        //找到button
 
-        Button loginButton = (Button) findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new Button.OnClickListener() {
+        //找到button
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        /*loginButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Click","Click the FB login buttom");
+                Log.i("Click","Click the FB login buttom");
                 LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile", "user_friends"));
+            }
+        });*/
+
+        //呼叫Button 操作
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                accessToken = AccessToken.getCurrentAccessToken();
+                Log.i("FB","Access Token Get!");
+                Log.i("FB",accessToken.toString());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Login Canceled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("FB","Login Error");
             }
         });
 
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode,data);
+    }
+
+/*
+
         //幫 LoginManager 增加callback function
-
         //這邊為了方便 直接寫成inner class
-
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
             //登入成功
-
             @Override
             public void onSuccess(LoginResult loginResult) {
 
                 //accessToken之後或許還會用到 先存起來
-                accessToken = loginResult.getAccessToken();
+
+                accessToken = AccessToken.getCurrentAccessToken();
+                //accessToken = loginResult.getAccessToken();
                 System.out.printf("TEXT" + accessToken.toString());
 
-                Log.d("FB", "access token got.");
-                //Log.d("FB",(accessToken.toString()));
+                Log.i("FB", "access token got.");
+                Log.i("FB",(accessToken.toString()));
                 //send request and call graph api
 
                 GraphRequest request = GraphRequest.newMeRequest(
@@ -112,24 +142,18 @@ public class MainActivity extends Activity {
             }
 
             //登入取消
-
             @Override
             public void onCancel() {
-                // App code
-
                 Log.d("FB", "CANCEL");
             }
 
             //登入失敗
-
             @Override
             public void onError(FacebookException exception) {
                 // App code
-
                 Log.d("FB", exception.toString());
             }
         });
     }
-
-
+*/
 }
